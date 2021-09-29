75f8340771a648f6b9d26386b3cccbaf7013a210
Adli Samy <sad@audaxis.com>
2021-09-29 11:46:37 +0100

    Refactoring files

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
-const {src, dest, watch, series} = require('gulp')
-const sass  = require('gulp-sass')(require('sass'))
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const purgeCss = require("gulp-purgecss");
 
-function buildStyles(){
-    return src('lib/**/*.scss')
-            .pipe(sass())
-            .pipe(dest('css'))
+function buildStyles() {
+  return src("sass/**/*.scss")
+    .pipe(sass())
+    .pipe(purgeCss({ content: ["*.html"] }))
+    .pipe(dest("css"));
 }
 
-function watchTask(){
-    watch(['lib/**/*.scss'],buildStyles)
+function watchTask() {
+  watch(["sass/**/*.scss","*.html"], buildStyles);
 }
 
-
-exports.default = series(buildStyles,watchTask)
+exports.default = series(buildStyles, watchTask);
